dac59cd8df1b78668f98c8516c91b73e16321417
willaCS <arthur.willems.prive@gmail.com>
2026-02-13 10:05:12 +0100

    added basic map example

--- CoreLogic/src/Map/Map.cs
+++ CoreLogic/src/Map/Map.cs
@@ -4,6 +4,43 @@ namespace CoreLogic;
 
 public record MapCell<T>(GameCell cell, T key);
 
-public class Map<T> {
-	private readonly UndirectedGraph<MapCell<T>, Edge<MapCell<T>>> map = new();
+public class Map<T> where T : notnull {
+	private readonly UndirectedGraph<T, Edge<T>> graph;
+	private readonly Dictionary<T, GameCell> cells;
+
+	public Map(
+		IEnumerable<(T key, GameCell cell)> cells,
+		IEnumerable<(T key1, T key2)> connexions
+	) {
+		ArgumentNullException.ThrowIfNull(cells);
+		ArgumentNullException.ThrowIfNull(connexions);
+
+		this.cells = [];
+		graph = new();
+
+		foreach ((T key, GameCell cell) in cells) {
+			this.cells.Add(key, cell);
+			_ = graph.AddVertex(key);
+		}
+
+		foreach ((T key1, T key2) in connexions) {
+			if (!graph.AddEdge(new Edge<T>(key1, key2))) {
+				throw new InvalidOperationException(
+					$"The cells {key1} and {key2} cannot be connected"
+				);
+			}
+		}
+	}
+
+	public GameCell getCell(T key) => cells[key];
+
+	public IEnumerable<(T key, GameCell cell)> getNeightbours(T key) {
+		foreach (Edge<T> edge in graph.AdjacentEdges(key)) {
+			T neightbour = edge.Source.Equals(key)
+				? edge.Target
+				: edge.Source;
+
+			yield return (neightbour, getCell(neightbour));
+		}
+	}
 }
--- CoreLogic/tests/Program.cs
+++ CoreLogic/tests/Program.cs
@@ -2,7 +2,31 @@ namespace CoreLogic.Tests;
 
 internal class Program {
 	private static void Main() {
-		Console.WriteLine("Tests");
-		Console.WriteLine("Passed 100%");
+		Console.WriteLine("\n\n\nStarting tests");
+
+		test();
+
+		Console.WriteLine("Ending tests");
+	}
+
+	private static void test() {
+		Map<uint> map = new(
+			[
+				(1, new GameCell()),
+				(2, new GameCell()),
+				(3, new GameCell()),
+				(4, new GameCell()),
+			],
+			[
+				(1, 2),
+				(2, 3),
+				(3, 4),
+				(4, 1),
+			]
+		);
+
+		foreach ((uint key, GameCell _) in map.getNeightbours(2)) {
+			Console.WriteLine($"{key}");
+		}
 	}
 }
