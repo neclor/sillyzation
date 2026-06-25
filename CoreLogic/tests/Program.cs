81df1ed1dab882fd2766ab05d3fccbdf52846087
willaCS <arthur.willems.prive@gmail.com>
2026-06-25 16:38:10 +0200

    ++

--- CoreLogic/tests/Program.cs
+++ CoreLogic/tests/Program.cs
@@ -118,17 +118,27 @@ internal class Program {
 				((2, 1), new Cell((2, 1), "Namur", Terrain.Forest, 3000000, [])),
 				((2, 2), new Cell((2, 2), "Liege", Terrain.Desert, 5000000, [])),
 				((1, 2), new Cell((1, 2), "Leuven", Terrain.Plain, 2000000, [])),
+				((1, 3), new Cell((1, 2), "Charleroi", Terrain.Savanna, 2000000, [])),
+				((2, 3), new Cell((1, 2), "Brugge", Terrain.Jungle, 2000000, [])),
+				((1, 4), new Cell((1, 2), "Charleroi", Terrain.Tundra, 2000000, [])),
+				((2, 4), new Cell((1, 2), "Brugge", Terrain.Jungle, 2000000, [])),
 			],
 			[
 				((1, 1), (2, 1)),
 				((2, 1), (2, 2)),
 				((2, 2), (1, 2)),
 				((1, 2), (1, 1)),
+				((1, 2), (1, 3)),
+				((1, 3), (1, 4)),
+				((2, 2), (2, 3)),
+				((2, 3), (2, 4)),
+				((1, 4), (2, 4)),
+				((1, 3), (2, 3)),
 			]
 		);
 
 
-		var app = new TerminalVersion(core, (2, 2));
+		var app = new TerminalVersion(core, (2, 4));
 		app.start();
 
 		// while (true) {
--- CoreLogic/tests/TerminalVersion.cs
+++ CoreLogic/tests/TerminalVersion.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using CoreLogic;
+using ErrorOr;
 
 internal class TerminalVersion {
 	private ICore core { get; }
@@ -7,6 +8,59 @@ internal class TerminalVersion {
 	private Dictionary<uint, IPlayer> players;
 
 	private static readonly (int x, int y) cell_size = (5, 3);
+	private const int minMenuWidth = 32;
+
+	private static readonly Dictionary<Terrain, string[]> backgrounds = new() {
+		{
+			Terrain.Plain, [
+				"\x1b[48;5;107m     \x1b[0m",
+				"\x1b[48;5;106m     \x1b[0m",
+				"\x1b[48;5;107m     \x1b[0m",
+			]
+		},
+		{
+			Terrain.Forest, [
+				"\x1b[38;5;22;48;5;28m ▲ ▲ \x1b[0m",
+				"\x1b[38;5;22;48;5;34m   ▲ \x1b[0m",
+				"\x1b[38;5;22;48;5;28m ▲   \x1b[0m",
+			]
+		},
+		{
+			Terrain.Desert, [
+				"\x1b[48;5;221m     \x1b[0m", 
+				"\x1b[48;5;222m     \x1b[0m",
+				"\x1b[48;5;214m     \x1b[0m",
+			]
+		},
+		{
+			Terrain.Tundra, [
+				"\x1b[48;5;253m     \x1b[0m",
+				"\x1b[48;5;195m     \x1b[0m",
+				"\x1b[48;5;254m     \x1b[0m",
+			]
+		},
+		{
+			Terrain.Savanna, [
+				"\x1b[48;5;142m     \x1b[0m",
+				"\x1b[48;5;136m     \x1b[0m",
+				"\x1b[48;5;142m     \x1b[0m", 
+			]
+		},
+		{
+			Terrain.Swamp, [
+				"\x1b[48;5;59m     \x1b[0m",
+				"\x1b[48;5;30m   █ \x1b[0m",
+				"\x1b[48;5;30m █   \x1b[0m",
+			]
+		},
+		{
+			Terrain.Jungle, [
+				"\x1b[38;5;22;48;5;34m ♣   \x1b[0m",
+				"\x1b[48;5;28m     \x1b[0m",
+				"\x1b[38;5;22;48;5;34m   ♣ \x1b[0m",
+			]
+		},
+	};
 
 	public TerminalVersion(ICore core, (int, int) map_size) {
 		this.core = core;
@@ -14,29 +68,47 @@ internal class TerminalVersion {
 		players = core.getAllPlayers();
 	}
 
-	private string[] printMap() {
-		return ["xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx", "xxxxxxxxxx"];
+	private List<string> printMap(IPlayer player) {
+		Terrain[][] terrains = new Terrain[map_size.x][];
+		for (uint x = 0; x < map_size.x; x++) {
+			terrains[x] = new Terrain[map_size.y];
+			for (uint y = 0; y < map_size.y; y++) {
+				ErrorOr<ICell> cell = core.getCell(player.id, (x + 1, y + 1));
+				Terrain terrain = cell.IsError ? Terrain.Plain : cell.Value.terrain;
+				terrains[x][y] = terrain;
+			}
+		}
+
+		List<string> res = [];
+		for (uint y = 0; y < map_size.y; y++) {
+			for (uint yc = 0; yc < cell_size.y; yc++) {
+				StringBuilder sb = new();
+				for (uint x = 0; x < map_size.x; x++) {
+					_ = sb.Append(backgrounds[terrains[x][y]][yc]);
+				}
+				res.Add(sb.ToString());
+			}
+		}
+		return res;
 	}
 
-	private void print(
-		string[] contentMenu
-	) {
+	private void print(string[] contentMenu, IPlayer player) {
 		int longest = (contentMenu.Length != 0)
 			? contentMenu.Max((cur) => cur.Length)
 			: 0;
-
+		int menuWidth = Math.Max(longest, minMenuWidth);
 		int mapWidth = map_size.x * cell_size.x;
 		int mapHeight = map_size.y * cell_size.y;
 		int nbLines = contentMenu.Length > mapHeight ? contentMenu.Length : mapHeight;
 
-		string[] map = printMap();
+		List<string> map = printMap(player);
 
 		StringBuilder sb = new();
-		_ = sb.Append('╔').Append('═', longest).Append('╦').Append('═', mapWidth).AppendLine("╗");
+		_ = sb.Append('╔').Append('═', menuWidth).Append('╦').Append('═', mapWidth).AppendLine("╗");
 		foreach ((string line, int i) in contentMenu.Select((value, index) => (value, index))) {
 			_ = sb
 				.Append('║')
-				.Append(line.PadRight(longest))
+				.Append(line.PadRight(menuWidth))
 				.Append('║');
 			if (i >= mapHeight) {
 				_ = sb
@@ -53,7 +125,7 @@ internal class TerminalVersion {
 			for (int i = contentMenu.Length; i < mapHeight; i++) {
 				_ = sb
 					.Append('║')
-					.Append(' ', longest)
+					.Append(' ', menuWidth)
 					.Append('║')
 					.Append(map[i])
 					.AppendLine("║");
@@ -61,7 +133,7 @@ internal class TerminalVersion {
 		}
 		_ = sb
 			.Append('╚')
-			.Append('═', longest)
+			.Append('═', menuWidth)
 			.Append('╩')
 			.Append('═', mapWidth)
 			.AppendLine("╝");
@@ -73,8 +145,9 @@ internal class TerminalVersion {
 	}
 
 	public void start() {
-		print(["Test", "Hello World"]);
-		// print(["Test", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World"]);
+		IPlayer testPlayer = players[1];
+		print(["Test", "Hello World"], testPlayer);
+		// print(["Test", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World"], testPlayer);
 		// while (true) {
 		// 	// foreach ((_, IPlayer player) in players) {
 		// 	// 	// printTurn(player, players, core);
